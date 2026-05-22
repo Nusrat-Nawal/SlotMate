f9f23ccfdc9a1bb4f199dda180f56d5c870e5a34
Nusrat Jahan Nawal <nusratnawal11@gmail.com>
2026-05-23 00:48:56 +0600

    Complete user registration with profile model and Django

--- slotmate_backend/slotmate_app/views.py
+++ slotmate_backend/slotmate_app/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render , redirect
+from .models import StudentProfile
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
 def register_page(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        university = request.POST.get("university")
+        department = request.POST.get("department")
+
+        username = email.split('@')[0]
+        
+        if User.objects.filter(username=username).exists():
+         return render(request, "register.html", {"error": "User already exists! Please use another email."} )
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        StudentProfile.objects.create(
+            user=user,
+            university=university,
+            department=department
+        )
+        return redirect('/login/')
+    
     return render(request, "register.html")
 def login_page(request):
     return render(request, "login.html")
