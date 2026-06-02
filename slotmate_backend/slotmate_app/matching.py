065e88fbd2a4652a8b0962652a500ac25395398b
Nusrat Jahan Nawal <nusratnawal11@gmail.com>
2026-06-03 05:03:10 +0600

    Update models,views, matching logic for create-request page

--- slotmate_backend/slotmate_app/matching.py
+++ slotmate_backend/slotmate_app/matching.py
@@ -6,33 +6,35 @@ def calculate_score(req1, req2):
         score += 35
 
     # SECTION (20)
-    if req2.any_section:
+    if req2.any_section or req2.preferred_section:
         score += 16
     elif req1.current_section == req2.preferred_section:
         score += 20
 
     # TIME (15)
-    if req2.any_time:
+    if req2.any_time or req2.preferred_time:
         score += 12
     elif req1.current_time == req2.preferred_time:
         score += 15
 
     # DAY (15)
-    if req2.any_day:
+    if req2.any_day or req2.preferred_days:
         score += 12
     else:
-        days1 = req1.current_days.split(",")
-        days2 = req2.preferred_days.split(",")
+        days1 = set((req1.current_days or "").split(","))
+        days2 = set((req2.preferred_days or "").split(","))
 
-        overlap = set(days1) & set(days2)
-
-        if len(overlap) == len(days2):
+        overlap = days1 & days2
+        
+        if not days2:
+             score += 12
+        elif len(overlap) == len(days2):
             score += 15
         elif len(overlap) > 0:
             score += 12
 
     # FACULTY (15)
-    if req2.any_faculty:
+    if req2.any_faculty or req2.preferred_faculty:
         score += 12
     elif req1.current_faculty == req2.preferred_faculty:
         score += 15
